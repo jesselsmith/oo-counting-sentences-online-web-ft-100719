3173e3ee3e59501e34760817fbc65abf0d1013d9
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-10 17:14:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    # self.split(/[.?!]+/).length
+    # self.split(/[.?!]+/).length is the simpler method with regex
     array = self.split(".")
     array = array.map {|sentences| sentences.split("?")}.flatten
     array = array.map{|sentences| sentences.split("!")}.flatten
